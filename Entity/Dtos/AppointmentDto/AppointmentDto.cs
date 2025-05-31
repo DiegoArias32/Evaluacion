ef2a698a7ef38c76e4dffc0bd9f2cdc4645a5af6
angelgarcia0 <garmiguelangel89@gmail.com>
2025-05-31 11:53:14 -0500

    Arreglos

---
+++ Entity/Dtos/AppointmentDto/AppointmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity.Dtos.AppointmentDto
+{
+    internal class AppointmentDto
+    {
+        public DateTime Date { get; set; }
+        public string Reason { get; set; }
+        public int PatientId { get; set; }
+        public int DoctorId { get; set; }
+    }
+}
